fdfd0060a5aafe6825c7f56337c0c88fdcb07c34
Mahmoud BanyAmer <Mahmoudbanyamer99@gmail.com>
2023-09-20 21:13:28 +0300

    init

---
+++ components/About.tsx
@@ -0,0 +1,74 @@
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import Image, { StaticImageData } from "next/image";
+import frontEndImage from "@/assets/imgs/front-end-v2.png";
+import backEndImage from "@/assets/imgs/backend.png";
+import threeDDesignerImg from "@/assets/imgs/3d designer.png";
+import { fadeIn, slideIn, staggerContainer } from "@/lib/motions";
+const About = () => {
+	type Card = {
+		title: string;
+		imageSrc: StaticImageData;
+	}[];
+	const cards: Card = [
+		{ title: "Frontend Developer", imageSrc: frontEndImage },
+		{ title: "Backend Developer", imageSrc: backEndImage },
+		{ title: "3D Designer", imageSrc: threeDDesignerImg },
+	];
+	return (
+		<motion.div className="h-screen sm:px-56 px-10   pt-20 ">
+			<motion.h1
+				variants={slideIn("up", "tween", 0.2, 1)}
+				initial="hidden"
+				whileInView={"show"}
+				className="font-extrabold text-xl  opacity-50 "
+			>
+				Introduction
+			</motion.h1>
+			<motion.h1
+				variants={slideIn("up", "tween", 0.2, 1)}
+				initial="hidden"
+				whileInView={"show"}
+				className=" text-5xl font-bold "
+			>
+				Overview
+			</motion.h1>
+			<motion.p
+				className="py-5"
+				variants={fadeIn("right", "tween", 0.2, 1)}
+				initial="hidden"
+				whileInView={"show"}
+			>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat, eum
+				dolore vero quam reprehenderit distinctio voluptates a odit aspernatur,
+				modi libero nemo quibusdam unde eligendi ipsum rem accusantium tenetur
+				magnam.
+			</motion.p>
+			<motion.div className="mt-10 grid sm:grid-cols-2 md:grid-cols-3 justify-center items-center gap-20">
+				{cards.map((card, index) => (
+					<motion.div
+						variants={slideIn("left", "tween", 0.6 * index, 1)}
+						initial="hidden"
+						whileInView={"show"}
+						viewport={{ once: false, amount: 0.25 }}
+						key={card.title}
+						className=" py-12 bg-accent px-20  rounded-lg flex flex-col justify-center items-center   "
+					>
+						<Image
+							src={card.imageSrc}
+							width={100}
+							height={100}
+							alt={card.title}
+							className="mb-5"
+						/>
+						<h3 className=" text-2xl font-bold">{card.title.split(" ")[0]}</h3>
+						<h3 className=" text-2xl font-bold">{card.title.split(" ")[1]}</h3>
+					</motion.div>
+				))}
+			</motion.div>
+		</motion.div>
+	);
+};
+
+export default About;
---
+++ components/Layout/Navbar.tsx
@@ -0,0 +1,72 @@
+"use client";
+import { AspectRatio } from "@radix-ui/react-aspect-ratio";
+import Link from "next/link";
+import React, { useState, useEffect } from "react";
+import { NavigationMenu } from "../ui/navigation-menu";
+import { ModeToggle } from "../ModeToggle";
+import { BiMenuAltRight } from "react-icons/bi";
+import { motion } from "framer-motion";
+import { navMenuVariantsSm, navMenuVariants } from "@/lib/motions";
+const Navbar = () => {
+	const [showMenu, setShowMenu] = useState<boolean>(true);
+	// Function to handle screen size changes and update showMenu state
+	const handleScreenSizeChange = () => {
+		if (window.innerWidth >= 640) {
+			setShowMenu(true);
+		}
+		if (window.innerWidth <= 640) {
+			setShowMenu(false);
+		}
+	};
+	useEffect(() => {
+		//To fix the issue where the menu remains hidden even when the screen resolution changes back to not being "sm"
+		window.addEventListener("resize", handleScreenSizeChange);
+		window.innerWidth <= 640 && setShowMenu(false);
+		return () => {
+			window.removeEventListener("resize", handleScreenSizeChange);
+		};
+	}, []);
+	return (
+		<nav className="z-10 py-5 flex sm:flex-row justify-between sm:px-56 px-10 bg-secondary sm:items-center  sticky top-0 flex-col ">
+			<h2 className="text-4xl font-extrabold ">MahmoudBA.</h2>
+			<BiMenuAltRight
+				cursor="pointer"
+				size={40}
+				className="sm:hidden hover:text-primary absolute top-5 right-5 "
+				onClick={() => {
+					setShowMenu((prev) => !prev);
+				}}
+			/>
+			<motion.div
+				variants={window.innerWidth < 640 ? navMenuVariants : navMenuVariantsSm}
+				initial="hidden"
+				whileInView={"show"}
+				className={`flex sm:flex-row flex-col gap-5 self-end     ${
+					showMenu ? "block" : "hidden"
+				} `}
+			>
+				<Link
+					href={"/"}
+					className="font-bold sm:text-3xl hover:text-primary transition-all hover:scale-110 duration-500  "
+				>
+					About
+				</Link>
+				<Link
+					href={"/"}
+					className="font-bold sm:text-3xl hover:text-primary transition-all hover:scale-110 duration-500  "
+				>
+					Works
+				</Link>
+				<Link
+					href={"/"}
+					className="font-bold sm:text-3xl hover:text-primary transition-all hover:scale-110 duration-500  "
+				>
+					Contact
+				</Link>
+				<ModeToggle />
+			</motion.div>
+		</nav>
+	);
+};
+
+export default Navbar;
