b18582a2c13cee310ac50c2f462902e7b1c1f7f5
Mahmoud BanyAmer <Mahmoudbanyamer99@gmail.com>
2023-09-21 02:32:47 +0300

    remove

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
+"use client";
 import "./globals.css";
 import type { Metadata } from "next";
 import { ThemeProvider } from "@/components/theme-provider";
 import Navbar from "@/components/Layout/Navbar";
 import { IBM_Plex_Mono } from "next/font/google";
-export const metadata: Metadata = {
+import { NextUIProvider } from "@nextui-org/react";
+
+const metadata: Metadata = {
 	title: "MahmoudBA",
 	description: "My portfolio made using nextjs, shadcn,framermotion",
 };
@@ -20,17 +23,20 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en" className={ipm_flex_mono.className}>
-			<body className="h-full">
-				<ThemeProvider
-					attribute="class"
-					defaultTheme="system"
-					enableSystem
-					disableTransitionOnChange
-				>
-					<Navbar />
-					{children}
-				</ThemeProvider>
-			</body>
+			<NextUIProvider>
+				<body className="h-full">
+					<ThemeProvider
+						attribute="class"
+						defaultTheme="system"
+						enableSystem
+						disableTransitionOnChange
+					>
+						{/* <Navbar /> */}
+
+						{children}
+					</ThemeProvider>
+				</body>
+			</NextUIProvider>
 		</html>
 	);
 }
